579326dbe7b2d2d6a070e31789b2c41f3f50be3f
VFC-nmorse <nathan.morse@covetrus.com>
2023-03-24 10:19:39 -0400

    last update

---
+++ backup_cp/code.py
@@ -0,0 +1,16 @@
+import asyncio
+import statemachine
+from blinky_states import blinky_states
+
+async def toggle_timer(rate):
+    while True:
+        
+        await asyncio.sleep(rate)
+        statemachine.enqueue("toggle")
+
+async def main():
+    toggle_timer_loop = asyncio.create_task(toggle_timer(0.5))
+    statemachine_loop = asyncio.create_task(statemachine.xstate_interpreter(blinky_states))
+    await asyncio.gather(toggle_timer_loop, statemachine_loop)
+
+asyncio.run(main())
